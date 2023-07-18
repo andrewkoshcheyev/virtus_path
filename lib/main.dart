074b53bec28a22953566e5e6c2c3cae9c116ab94
andrewkoshcheyev <andrewfanera@gmail.com>
2023-07-18 21:14:23 +0800

    added database

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,32 @@ import 'screens/settings_screen.dart';
 import 'models/quote.dart';
 import 'screens/draft_screen01.dart';
 import 'screens/draft_screen02.dart';
+import 'data/quotes_data.dart'; // import the quotes data
+import 'helpers/database_helper.dart'; // import the database helper
 
 void main() {
   runApp(VirtusPathApp());
 }
 
 class VirtusPathApp extends StatelessWidget {
+  // Create an instance of the database helper
+  final dbHelper = DatabaseHelper.instance;
+
   @override
   Widget build(BuildContext context) {
+    // Clear the database
+    dbHelper.clear();
+
+    // Populate the database with the quotes data
+    for (var quote in quotes) {
+      dbHelper.insert({
+        DatabaseHelper.columnText: quote.text,
+        DatabaseHelper.columnAuthor: quote.author,
+        DatabaseHelper.columnImagePath: quote.imagePath,
+        DatabaseHelper.columnCategories: quote.categories.join(','),
+      });
+    }
+
     return MaterialApp(
       title: 'Virtus Path',
       theme: ThemeData(
@@ -25,104 +43,17 @@ class VirtusPathApp extends StatelessWidget {
         errorColor: Color(0xFFEE5622),
         cardColor: Color(0xFFE5E5E5),
         textTheme: TextTheme(
-          bodyText2: TextStyle(color: Color(0xFFE5E5E5)), // Default text color
+          bodyText2: TextStyle(color: Color(0xFFE5E5E5)),
         ),
       ),
       initialRoute: '/',
       routes: {
-        //!obsolete?
         '/': (context) => HomeScreen(),
         '/settings': (context) => SettingsScreen(),
-        /*'/quoteDetail': (context) => QuoteDetailScreen(),*/
         '/challenges': (context) => Draft02Screen(),
         '/categories': (context) => CategoriesScreen(),
         '/favorites': (context) => FavoritesScreen(),
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
