ada2fb3f3304210b50baa66ae40c55c3283680ee
andrewkoshcheyev <andrewfanera@gmail.com>
2023-07-19 19:47:38 +0800

    git add

--- lib/data/quotes_data.dart
+++ lib/data/quotes_data.dart
@@ -21,4 +21,51 @@ List<Quote> quotes = [
       author: "Unknown",
       imagePath: 'assets/images/image3.jpg',
       categories: ['Health', 'Family']),
+  Quote(
+    id: 4,
+    text:
+        "Do not anticipate trouble, or worry about what may never happen. Keep in the sunlight.",
+    author: "Benjamin Franklin",
+    imagePath: "assets/images/image4.jpg",
+    categories: ["Motivation", "Life"],
+  ),
+  Quote(
+    id: 5,
+    text:
+        "Anxiety does not empty tomorrow of its sorrows, but only empties today of its strength.",
+    author: "Charles Spurgeon",
+    imagePath: "assets/images/image5.jpg",
+    categories: ["Motivation", "Life"],
+  ),
+  Quote(
+    id: 6,
+    text:
+        "The difference between ordinary and extraordinary is that little extra.",
+    author: "Jimmy Johnson",
+    imagePath: "assets/images/image6.jpg",
+    categories: ["Motivation", "Success"],
+  ),
+  Quote(
+    id: 7,
+    text: "It’s not about perfect. It’s about effort.",
+    author: "Jillian Michaels",
+    imagePath: "assets/images/image7.jpg",
+    categories: ["Motivation", "Work"],
+  ),
+  Quote(
+    id: 8,
+    text:
+        "The only way to achieve the impossible is to believe it is possible.",
+    author: "Charles Kingsleigh",
+    imagePath: "assets/images/image8.jpg",
+    categories: ["Motivation", "Inspiration"],
+  ),
+  Quote(
+    id: 9,
+    text:
+        "The only thing standing between you and your goal is the story you keep telling yourself as to why you can't achieve it.",
+    author: "Jordan Belfort",
+    imagePath: "assets/images/image4.jpg",
+    categories: ["Motivation", "Success"],
+  ),
 ];
