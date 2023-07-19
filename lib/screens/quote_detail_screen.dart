37efb0aa6dfed10ad296dc1c091716caee19547f
andrewkoshcheyev <andrewfanera@gmail.com>
2023-07-19 17:11:39 +0800

    git add

--- lib/screens/quote_detail_screen.dart
+++ lib/screens/quote_detail_screen.dart
@@ -57,89 +57,115 @@ class _QuoteDetailScreenState extends State<QuoteDetailScreen> {
     Quote currentQuote = quotes[currentIndex];
 
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(currentQuote.imagePath),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-              child: Container(
-                color: Colors.black.withOpacity(0.6),
-                child: SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(Icons.arrow_back, color: Colors.white),
-                          onPressed: () => Navigator.of(context).pop(),
-                        ),
-                        Expanded(
-                          flex: 7,
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  currentQuote.text,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(currentQuote.imagePath),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+          child: Container(
+            color: Colors.black.withOpacity(0.6),
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back, color: Colors.white),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                    Expanded(
+                      flex: 7,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 5.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconButton(
+                                    icon: Icon(Icons.arrow_back_ios,
+                                        color: Colors.white),
+                                    onPressed: () => goToPreviousQuote(),
                                   ),
-                                  textAlign: TextAlign.center,
-                                ),
-                                SizedBox(height: 8),
-                                Align(
-                                  alignment: Alignment.centerRight,
-                                  child: Text(
-                                    "- " + currentQuote.author,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.normal,
+                                  Expanded(
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          currentQuote.text,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                        SizedBox(height: 8),
+                                        Align(
+                                          alignment: Alignment.centerRight,
+                                          child: Text(
+                                            "- " + currentQuote.author,
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.normal,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                ),
-                              ],
+                                  IconButton(
+                                    icon: Icon(Icons.arrow_forward_ios,
+                                        color: Colors.white),
+                                    onPressed: () => goToNextQuote(),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
+                          ],
                         ),
-                        Expanded(
-                          flex: 3,
-                          child:
-                              Container(), // Add your icons or other widgets here.
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Icon(Icons.favorite,
+                                  color: Colors.white, size: 30),
+                              Icon(Icons.share, color: Colors.white, size: 30),
+                              Icon(Icons.edit, color: Colors.white, size: 30),
+                            ],
+                          ),
+                          SizedBox(height: 16),
+                          Text(
+                            'Categories: ' + currentQuote.categories.join(', '),
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Text('Open Categories'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-          Positioned(
-            left: 10,
-            top: MediaQuery.of(context).size.height * 0.35,
-            child: IconButton(
-              icon: Icon(Icons.arrow_back_ios, color: Colors.white),
-              onPressed: () => goToPreviousQuote(),
-            ),
-          ),
-          Positioned(
-            right: 10,
-            top: MediaQuery.of(context).size.height * 0.35,
-            child: IconButton(
-              icon: Icon(Icons.arrow_forward_ios, color: Colors.white),
-              onPressed: () => goToNextQuote(),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
