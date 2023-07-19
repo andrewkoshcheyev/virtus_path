5dab0a3ebfd80f7acdbd83a2aa972941e52f5b3a
andrewkoshcheyev <andrewfanera@gmail.com>
2023-07-19 14:17:22 +0800

    git add

--- lib/screens/quote_detail_screen.dart
+++ lib/screens/quote_detail_screen.dart
@@ -2,116 +2,93 @@ import 'package:flutter/material.dart';
 import '../models/quote.dart';
 import 'dart:ui';
 
-class QuoteDetailScreen extends StatelessWidget {
+class QuoteDetailScreen extends StatefulWidget {
   final Quote quote;
+  final Function onNext;
+  final Function onPrevious;
 
-  QuoteDetailScreen({required this.quote});
+  QuoteDetailScreen(
+      {required this.quote, required this.onNext, required this.onPrevious});
 
+  @override
+  _QuoteDetailScreenState createState() => _QuoteDetailScreenState();
+}
+
+class _QuoteDetailScreenState extends State<QuoteDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 6,
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(quote.imagePath),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-                child: Container(
-                  color: Colors.black.withOpacity(0.6),
-                  child: SafeArea(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          IconButton(
-                            icon: Icon(Icons.arrow_back, color: Colors.white),
-                            onPressed: () => Navigator.of(context).pop(),
-                          ),
-                          Expanded(
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    quote.text,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                  SizedBox(height: 8),
-                                  Align(
-                                    alignment: Alignment.centerRight,
-                                    child: Text(
-                                      "- " + quote.author,
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.normal,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(widget.quote.imagePath),
+            fit: BoxFit.cover,
           ),
-          Expanded(
-            flex: 4,
-            child: Container(
-              color: Colors.black.withOpacity(0.6),
+        ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+          child: Container(
+            color: Colors.black.withOpacity(0.6),
+            child: SafeArea(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
                     IconButton(
-                      iconSize: 30.0,
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.format_quote,
-                        color: Colors.white,
+                      icon: Icon(Icons.arrow_back, color: Colors.white),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              widget.quote.text,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: 8),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                "- " + widget.quote.author,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    IconButton(
-                      iconSize: 30.0,
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.favorite_border,
-                        color: Colors.white,
-                      ),
-                    ),
-                    IconButton(
-                      iconSize: 30.0,
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.more_horiz,
-                        color: Colors.white,
-                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+                          onPressed: () => widget.onPrevious(),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.arrow_forward_ios,
+                              color: Colors.white),
+                          onPressed: () => widget.onNext(),
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
